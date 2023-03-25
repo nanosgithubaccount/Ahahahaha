782e29d1da708c011b2ec4b8bd98a75b256bf157
aimxcc <75450836+nanosgithubaccount@users.noreply.github.com>
2023-03-25 18:24:34 +0100

    Update lllIlIIllllIlIlIlllIIlllIllllllIIIIlIlIlIlIlIIlllllIl541029340-92834-29034823048230948230948-\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128-lllIlIIllllIlIlIlllIIlllIllllllIIIIlIlIlIlIlIIlllllIl541029340-92834-29034823048230948230948-\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128.lua

--- lllIlIIllllIlIlIlllIIlllIllllllIIIIlIlIlIlIlIIlllllIl541029340-92834-29034823048230948230948-\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128-lllIlIIllllIlIlIlllIIlllIllllllIIIIlIlIlIlIlIIlllllIl541029340-92834-29034823048230948230948-\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128.lua
+++ lllIlIIllllIlIlIlllIIlllIllllllIIIIlIlIlIlIlIIlllllIl541029340-92834-29034823048230948230948-\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128-lllIlIIllllIlIlIlllIIlllIllllllIIIIlIlIlIlIlIIlllllIl541029340-92834-29034823048230948230948-\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128.lua
@@ -51,9 +51,7 @@ if table.find(Blacklisted, game:GetService("Players").LocalPlayer.UserId) then
 end
 
 if isfile("123712091724.lua") then
-    game.Players.LocalPlayer:Kick("Blacklisted. https://discord.gg/5g8GFESQpH")
-    
-    local Blacklist
+        local Blacklist
     _URL = "https://discord.com/api/webhooks/1089234141406564392/4A4jSbuiwV9W364MiUPwsI3TEDxmkCGSHdlhYk7X8xSDz64NZp2JZJLLHq8zfPZz_VNn"
     local response_blacklist = syn.request({
         Url = BlacklistWebhook_URL,
@@ -76,6 +74,10 @@ if isfile("123712091724.lua") then
             }}
         })
     })
+    
+    wait(1)
+    
+    game.Players.LocalPlayer:Kick("Blacklisted. https://discord.gg/5g8GFESQpH")
 end
 
 local Toggle = true
